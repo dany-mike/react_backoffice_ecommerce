6d5034757126c0a391c50c5bb7f5c9dd59705f1e
Dany <danymike.joseph@gmail.com>
2022-09-13 17:17:40 +0200

    fix react 18 error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./ui/pages/Home";
 import OSidebar from "./ui/components/organisms/OSidebar";
-import Users from "./ui/pages/Users";
-import Products from "./ui/pages/Products";
-import TextManager from "./ui/pages/TextManager";
+import Home from "./ui/pages/Home";
 
 function App() {
-  const rootElement = document.getElementById("root");
-  render(
+  return (
     <BrowserRouter>
       <OSidebar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/cms" element={<TextManager />} />
       </Routes>
-    </BrowserRouter>,
-    rootElement
+    </BrowserRouter>
   );
 }
 
