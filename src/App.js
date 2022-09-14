b0c63fe254ca79c832d2a40edd04954c033edcc0
Dany <danymike.joseph@gmail.com>
2022-09-14 16:34:34 +0200

    feature: add navbar backoffice

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from "./ui/pages/Home";
 import Products from "./ui/pages/Products";
 import TextManager from "./ui/pages/TextManager";
 import Users from "./ui/pages/Users";
-import Sidebar from "./ui/components/Sidebar";
+import Sidebar from "./ui/components/Sidebar/Sidebar";
+import Navbar from "./ui/components/Navbar/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <div className="flex h-screen">
         <Sidebar />
         <div className="content overflow-x-hidden overflow-scroll w-full p-4 lg:p-12">
