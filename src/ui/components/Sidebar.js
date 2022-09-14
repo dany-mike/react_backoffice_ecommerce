37db0446c756c2ddf4cfa95b4cfc4efdb5871ed8
Dany <danymike.joseph@gmail.com>
2022-09-14 16:04:42 +0200

    feature: mock pages data

--- src/ui/components/Sidebar.js
+++ src/ui/components/Sidebar.js
@@ -1,10 +1,31 @@
-import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import Hamburger from "hamburger-react";
 import "react-pro-sidebar/dist/css/styles.css";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   const [isCollapsed, setCollapsed] = useState(true);
+
+  // TODO fetch pages from API cms module
+  const boPages = [
+    {
+      label: "Home",
+      path: "/",
+    },
+    {
+      label: "Products",
+      path: "/products",
+    },
+    {
+      label: "Users",
+      path: "/users",
+    },
+    {
+      label: "Text Manager",
+      path: "/cms",
+    },
+  ];
 
   return (
     <ProSidebar collapsed={isCollapsed}>
@@ -21,11 +42,12 @@ function Sidebar() {
         />
       </div>
       <Menu iconShape="square">
-        <MenuItem>Dashboard</MenuItem>
-        <SubMenu title="Components">
-          <MenuItem>Component 1</MenuItem>
-          <MenuItem>Component 2</MenuItem>
-        </SubMenu>
+        {boPages.map((item, index) => (
+          <MenuItem key={index}>
+            {item.label}
+            <Link to={item.path} />
+          </MenuItem>
+        ))}
       </Menu>
     </ProSidebar>
   );
