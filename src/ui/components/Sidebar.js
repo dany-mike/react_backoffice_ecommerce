b079e16c8180b85279191b86086bfe28a241689f
Dany <danymike.joseph@gmail.com>
2022-09-14 15:52:33 +0200

    feature: add collapsed implementation sidebar

--- src/ui/components/Sidebar.js
+++ src/ui/components/Sidebar.js
@@ -1,9 +1,25 @@
 import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import Hamburger from "hamburger-react";
 import "react-pro-sidebar/dist/css/styles.css";
+import { useState } from "react";
 
 function Sidebar() {
+  const [isCollapsed, setCollapsed] = useState(true);
+
   return (
-    <ProSidebar>
+    <ProSidebar collapsed={isCollapsed}>
+      <div className="flex justify-center py-8 lg:hidden">
+        <Hamburger
+          onToggle={(collapsed) => {
+            if (collapsed) {
+              setCollapsed(false);
+            } else {
+              setCollapsed(true);
+            }
+          }}
+          size={20}
+        />
+      </div>
       <Menu iconShape="square">
         <MenuItem>Dashboard</MenuItem>
         <SubMenu title="Components">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+// TODO: change lg breakpoint value to 992px
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      lg: "992px",
+    },
     extend: {},
   },
   plugins: [],
