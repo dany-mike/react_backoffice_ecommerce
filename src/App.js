670cb396b8d0ffb27a35ec82352e2267a7d648ab
Dany <danymike.joseph@gmail.com>
2022-09-14 14:45:48 +0200

    feature: sidebar added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Home from "./ui/pages/Home";
 import Products from "./ui/pages/Products";
 import TextManager from "./ui/pages/TextManager";
 import Users from "./ui/pages/Users";
+import Sidebar from "./ui/components/Sidebar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/cms" element={<TextManager />} />
-      </Routes>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="content overflow-x-hidden overflow-scroll w-full">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cms" element={<TextManager />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
