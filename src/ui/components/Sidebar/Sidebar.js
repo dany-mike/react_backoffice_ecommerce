c990a831a34b314ce421ec1e591f116fc4edf397
Dany <danymike.joseph@gmail.com>
2022-09-15 17:53:42 +0200

    feature: handle responsive

--- src/ui/components/Sidebar/Sidebar.js
+++ src/ui/components/Sidebar/Sidebar.js
@@ -4,6 +4,7 @@ import "react-pro-sidebar/dist/css/styles.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { isMobile } from "react-device-detect";
+import Button from "../Button/Button";
 
 function Sidebar() {
   const stateValue = isMobile ? true : false;
@@ -44,6 +45,20 @@ function Sidebar() {
         />
       </div>
       <Menu iconShape="square">
+        <MenuItem>
+          <Link to="/login">
+            <Button classNameValue="lg:hidden flex justify-center w-full bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+              Login
+            </Button>
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/register">
+            <Button classNameValue="lg:hidden flex justify-center bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+              Register
+            </Button>
+          </Link>
+        </MenuItem>
         {boPages.map((item, index) => (
           <MenuItem key={index}>
             {item.label}
